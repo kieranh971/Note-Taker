cbfac5fe8aba1c09377492d3972cfbc36bb9e2d2
Kieran Harris <kharris19063@gmail.com>
2021-11-17 19:11:11 -0500

    Added first POST request

--- server.js
+++ server.js
@@ -6,6 +6,44 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// HTML pages
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+// Post request
+app.post("/api/notes", (req,res) => {
+    fs.readFile("./db/db.json", `utf8`, (err, data) => {
+        if (err) throw err;
+        // Declaring note variables
+        var notes = JSON.parse(data);
+        var id = notes[notes.length - 1].id + 1;
+        // Object variables for notes
+        var createNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: id
+        }
+        // Combines active notes with newly created notes
+        var currentNote = notes.concat(createNote)
+
+        fs.writeFile(__dirname + "./db/db.json",JSON.stringify(createNote), (err, data) => {
+            if (err) throw err;
+            console.log(currentNote);
+            res.json(currentNote)
+        })
+    })
+});
+
 // Starts server
 app.listen(PORT, () => {
     console.log(`Listening for server at ${PORT}`)
